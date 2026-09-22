4c9544ea47e35722a117f9cba9e347ffc0185d92
Tod <tpoirier@nmu.edu>
2026-09-22 15:42:38 -0400

    Refresh quick-range state after manual dates

--- src/app.js
+++ src/app.js
@@ -546,6 +546,7 @@ function validateAndStoreRange() {
   state.rangeMode="custom";
   state.rangeNoticeArmed=true;
   clearTrendZoom();
+  renderTrendQuickRanges();
   persistUiState();
   return true;
 }
