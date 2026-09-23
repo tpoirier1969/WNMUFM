b669a4aaf92cf12a078de9167aa9820cd6b40500
Tod <tpoirier@nmu.edu>
2026-09-23 09:31:56 -0400

    Update recurrence regression expectation for genre metadata

--- tests/schedule-recurrence.test.mjs
+++ tests/schedule-recurrence.test.mjs
@@ -8,7 +8,7 @@ test("Composer recurring programs expand into dated weekly airtimes", () => {
     recurrences:[{days:"Saturday",start:"16:00",end:"17:00"},{days:"Monday",start:"14:00",end:"15:00"}]
   }], "2026-09-14", "2026-09-20");
   assert.deepEqual(rows, [
-    {date:"2026-09-14",start:"14:00",end:"15:00",program:"The Shuffle"},
-    {date:"2026-09-19",start:"16:00",end:"17:00",program:"The Shuffle"}
+    {date:"2026-09-14",start:"14:00",end:"15:00",program:"The Shuffle",genre:""},
+    {date:"2026-09-19",start:"16:00",end:"17:00",program:"The Shuffle",genre:""}
   ]);
 });
